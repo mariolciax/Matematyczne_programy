002c4c0cd985b7568447628fbc5e59df4417406a
Mariola Bartosik <56107837+mariolciax@users.noreply.github.com>
2021-02-26 15:57:27 +0100

    Update Inverse_of_a_triangular_matrix.m

--- Inverse_of_a_triangular_matrix.m
+++ Inverse_of_a_triangular_matrix.m
@@ -1,17 +1,16 @@
 function [X, info] = invU(A)
-% Znajdowanie odwrotności X macierzy trójkątnej górnej A
-% wywołanie [X, info] = invU(A)
-% X poprawna, gdy info=1; wpp info=0
+% Finding inverse of a triangular matrix
+% X correct, when info=1; other info=0
 EPS=1e-10;
 [m,n] = size(A);
-X = A; %dzięki temu znamy rozmiar
+X = A; 
 info = 1;
 if m ~= n || any(abs(diag(A)) < EPS)
     info = 0;
     % X = A;
     return;
 end
-X=diag(1./diag(A)); %macierz diagonalna z diagonalna rowna 1./diag(A), czyli odwrotności poszczególnych elementow
+X=diag(1./diag(A)); %diagonal matrix 
 for i=1:n
     for j = i + 1:n
         X(i,j) = -X(i,i:j-1)*A(i:j-1,j)/A(j,j);
@@ -20,7 +19,7 @@ end
 end
 
 % for i=1:n
-%     X(i,i) = 1/A(i,i); %el indeksujemy od jedynki
+%     X(i,i) = 1/A(i,i); % index el from 1
 %     for j = i + 1:n
 %         s = 0;
 %         for k = i:j-1
