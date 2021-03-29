1ed994ad53c3b03f5a7fbd4fb73f4896cd81382a
Mariola Bartosik <56107837+mariolciax@users.noreply.github.com>
2021-03-29 13:30:21 +0200

    Update DET.m

--- DET.m
+++ DET.m
@@ -1,5 +1,5 @@
 function det = metGE(A)
-    %Obliczanie wartości wyznacznika macierzy trójdiagonalnej 
+    %Obliczanie wartości wyznacznika macierzy  
     %zastosowanie metody eliminacji Gaussa (GE)
     % Wywołanie: det = metGE(A)
     % WEJ: A- macierz trójdiagonalna
@@ -57,3 +57,42 @@ function det = metGE(A)
         det = det * B(i, i);
     end 
     end
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% ostateczna
+
+function det = metGE(A)
+%Obliczanie wartości wyznacznika macierzy trójdiagonalnej 
+%zastosowanie metody eliminacji Gaussa (GE)
+% Wywołanie: det = metGE(A)
+% Wejście: A- macierz trójdiagonalna
+% Wyjście: det - wyznacznik badanej macierzy
+
+if A(1,1) == 0
+    det = "Metoda nie jest wykonalna";
+    return
+end 
+
+if size(A,1) ~= size(A,2) %sprawdzamy przypadek czy została podana macierz kwadratowa
+    det = "To nie jest macierz kwadratowa";
+    return
+end 
+
+n = size(A,1);
+B = A;
+
+for j = 2 : n  % wiersze
+    
+    if B(j-1, j-1) ~= 0
+        wsp = B(j, j-1)/B(j-1, j-1); %obliczam wspólczynnik
+        B(j, j-1:j) = B(j, j-1:j) - wsp .* B(j-1, j-1:j); % od elementów j-1, j z wiersza j odejmuje elementy j-1, j z wiersza j-1 
+    end
+    
+    if B(j-1, j-1) == 0
+        det = "Metoda nie jest wykonalna";
+        return
+    end
+end
+det = B(1, 1);
+for i=2:n  % liczymy wyznacznik
+    det = det * B(i, i);
+end 
+end
