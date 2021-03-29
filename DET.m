b0ff8b682f28d2bf59a1926fbb8d19b560334e87
Mariola Bartosik <56107837+mariolciax@users.noreply.github.com>
2021-03-29 13:06:35 +0200

    Update DET.m

--- DET.m
+++ DET.m
@@ -4,27 +4,27 @@ function det = metGE(A)
 % Wywołanie: det = metGE(A)
 % WEJ: A- macierz trójdiagonalna
 % Wyj: det - wyznacznik badanej macierzy
-if A(1,1) == 0
-    det = "Metoda nie jest wykonalna";
-    return
-end 
+    if A(1,1) == 0
+        det = "Metoda nie jest wykonalna";
+        return
+    end 
 
-if size(A,1) ~= size(A,2) %sprawdzamy przypadek czy została podana macierz kwadratowa
-    det = "To nie jest macierz kwadratowa";
-    return
-end 
+    if size(A,1) ~= size(A,2) %sprawdzamy przypadek czy została podana macierz kwadratowa
+        det = "To nie jest macierz kwadratowa";
+        return
+    end 
 
-n = size(A,1);
-B = A;
+    n = size(A,1);
+    B = A;
 
-for j = 2 : n  % wiersze
-    wsp = B(j, j-1)/B(j-1, j-1); %obliczam wspólczynnik
-    %for i = 1 : n  %kolumny  
-        B(j, 1 : n) = B(j, 1 : n) - wsp .* B(j-1, 1 : n); % od każdego elementu z wiersza j odejmuje element z wiersza j-1 
-    %end
-end
-det = 1;
-for i=1:n  % liczymy wyznacznik
-    det = det * B(i, i);
-end 
+    for j = 2 : n  % wiersze
+        wsp = B(j, j-1)/B(j-1, j-1); %obliczam wspólczynnik
+        %for i = 1 : n  %kolumny  
+            B(j, 1 : n) = B(j, 1 : n) - wsp .* B(j-1, 1 : n); % od każdego elementu z wiersza j odejmuje element z wiersza j-1 
+        %end
+    end
+    det = 1;
+    for i=1:n  % liczymy wyznacznik
+        det = det * B(i, i);
+    end 
 end
