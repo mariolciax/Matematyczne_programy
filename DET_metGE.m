3e11f012bc867e79d03b80df8dace5922d7a673a
Mariola Bartosik <56107837+mariolciax@users.noreply.github.com>
2021-04-02 19:04:04 +0200

    Update DET_metGE.m

--- DET_metGE.m
+++ DET_metGE.m
@@ -59,40 +59,39 @@ function det = metGE(A)
     end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% ostateczna
 
-function det = metGE(A)
-%Obliczanie wartości wyznacznika macierzy trójdiagonalnej 
-%zastosowanie metody eliminacji Gaussa (GE)
-% Wywołanie: det = metGE(A)
-% Wejście: A- macierz trójdiagonalna
-% Wyjście: det - wyznacznik badanej macierzy
+function det_m = metGE(A)
+    %Obliczanie wartości wyznacznika macierzy trójdiagonalnej 
+    %zastosowanie metody eliminacji Gaussa (GE)
+    % Wywołanie: det = metGE(A)
+    % Wejście: A- macierz trójdiagonalna
+    % Wyjście: det - wyznacznik badanej macierzy
 
-if A(1,1) == 0
-    det = "Metoda nie jest wykonalna";
-    return
-end 
-
-if size(A,1) ~= size(A,2) %sprawdzamy przypadek czy została podana macierz kwadratowa
-    det = "To nie jest macierz kwadratowa";
-    return
-end 
-
-n = size(A,1);
-B = A;
-
-for j = 2 : n  % wiersze
-    
-    if B(j-1, j-1) ~= 0
-        wsp = B(j, j-1)/B(j-1, j-1); %obliczam wspólczynnik
-        B(j, j-1:j) = B(j, j-1:j) - wsp .* B(j-1, j-1:j); % od elementów j-1, j z wiersza j odejmuje elementy j-1, j z wiersza j-1 
-    end
-    
-    if B(j-1, j-1) == 0
-        det = "Metoda nie jest wykonalna";
+    if A(1,1) == 0
+        det_m = "Metoda nie jest wykonalna";
         return
+    end 
+
+    if size(A,1) ~= size(A,2) %sprawdzamy przypadek czy została podana macierz kwadratowa
+        det_m = "To nie jest macierz kwadratowa";
+        return
+    end 
+
+    n = size(A,1);
+
+    for j = 2 : n  % wiersze    
+        if A(j-1, j-1) ~= 0
+            wsp = A(j, j-1)/A(j-1, j-1); %obliczam wspólczynnik, ktory pozwoli wyzerować element
+            A(j, j-1:j) = A(j, j-1:j) - wsp .* A(j-1, j-1:j); % od elementów j-1, j z wiersza j odejmuje elementy j-1, j z wiersza j-1 
+        end
+
+        if A(j-1, j-1) == 0
+            det_m = 'Metoda nie jest wykonalna';
+            return
+        end
     end
+    det_m = A(1, 1);
+    for i=2:n  % liczymy wyznacznik
+        det_m = det_m * A(i, i);
+    end 
 end
-det = B(1, 1);
-for i=2:n  % liczymy wyznacznik
-    det = det * B(i, i);
-end 
-end
+
