d2c7e179034ba1aa9c9b79786841ba78556640ff
Mariola Bartosik <56107837+mariolciax@users.noreply.github.com>
2021-04-02 14:10:51 +0200

    Update testy_.m

--- testy_.m
+++ testy_.m
@@ -6,33 +6,32 @@ function spr = test_(n, m)
     % sprawdzenia; 
     % m - wybór metody przez podanie wartosci, w przypadku 0 to met_GE przez podanie 1 to metMINOR 
     % Wyjście: spr - wynik testu w skali od 1 do n
-tol = 1e-14;
-if n <= 0
-    spr = "0 macierzy";
-    return
-end
-licz = 0;
+    tol = 1e-14;
+    if n <= 0
+        spr = "0 macierzy";
+        return
+    end
+    licz = 0;
 
-if m == 0
-    for i = 1:n
-        w = randi(30); % wymiar generowanej macierzy
-        macierz = full(gallery('tridiag', w, rand(1, w-1), rand(1,w), rand(1, w-1)));  % generowanie takiej macierzy
-        if abs(det(macierz) - metGE(macierz)) < tol
-            licz = licz + 1;
-        end
-     end   
-end
+    if m == 0
+        for i = 1:n
+            w = randi(30); % wymiar generowanej macierzy
+            macierz = full(gallery('tridiag', w, rand(1, w-1), rand(1,w), rand(1, w-1)));  % generowanie takiej macierzy
+            if abs(det(macierz) - metGE(macierz)) < tol
+                licz = licz + 1;
+            end
+         end   
+    end
 
-if m == 1
-    for i = 1:n
-        w = randi(30); % wymiar generowanej macierzy
-        macierz = full(gallery('tridiag', w, rand(1, w-1), rand(1,w), rand(1, w-1))); 
-        if abs(det(macierz) - metMINOR(macierz)) < tol
-            licz = licz + 1;
-        end
-        
-    end   
-end
-spr = fprintf('Wynik testu: %d  /%d.', licz, n);
+    if m == 1
+        for i = 1:n
+            w = randi(30); % wymiar generowanej macierzy
+            macierz = full(gallery('tridiag', w, rand(1, w-1), rand(1,w), rand(1, w-1))); 
+            if abs(det(macierz) - metMINOR(macierz)) < tol
+                licz = licz + 1;
+            end
 
+        end   
+    end
+    spr = fprintf('Wynik testu: %d  /%d.', licz, n);
 end
